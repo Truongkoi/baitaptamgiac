93594e8f2ee8b34c19dd6c067e68fd59f46a03fb
truongkoi <truongmc1996@gmail.com>
2015-03-31 08:35:07 -0700

    baitap

---
+++ tamgiac1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	int d, i, j;
+	cout << "nhap vao d : ";
+	cin >> d; cin.ignore();
+	for (j = d; j >= 1; j--)
+	{
+		for (i = j; i <= d; i++)cout << "* ";
+		cout << "\n";
+	}
+	cin.get();
+}
